c8e4c0996e4e697c47c8186c6705e032fa906fa8
Nathan <nharvey1990@gmail.com>
2022-06-30 00:13:52 -0500

    Added unit test

---
+++ war_test.js
@@ -0,0 +1,13 @@
+//Unit tests
+
+var expect = chai.expect
+
+describe('Function test', function() {
+    describe('#getCardValue', function(){
+        it('Should return the value of the passed in card', function(){
+            var game = new Game()
+            let x = game.getCardValue('K')
+            expect(x).to.equal(13)
+        })
+    })
+})
